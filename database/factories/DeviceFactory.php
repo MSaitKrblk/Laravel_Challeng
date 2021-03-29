dad1c84798c63f2c89e6a6bc6fefbce222d78a1b
skarabulak <skarabulak@eraysoft.com.tr>
2021-03-29 09:35:07 +0300

    Initial Commit

---
+++ database/factories/DeviceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Device;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DeviceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Device::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'uid' => $this->faker->uuid(),
+            'appId' => $this->faker->uuid(),
+            'language' => $this->faker->word(),
+            'operatingSystem'=> $this->faker->userAgent(),
+            //
+        ];
+    }
+}
